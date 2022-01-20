653b74701d22361bc1efff3204c5c7d203965ecc
岡田 諒 <okadaryo@okadanoMacBook-Pro.local>
2022-01-20 17:12:30 +0900

    practice

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("にんじゃわんこ");
+
+console.log(5 + 3);
+console.log(20 - 8);
+console.log("4 + 5");
+
+console.log(8 * 4);
+console.log(24 / 4);
+console.log(7 % 2);
