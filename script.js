74030da0ce6ee432683636a5a396e0a66171640f
ryooookada <btbl70120@gmail.com>
2022-01-27 19:44:34 +0900

    practice

--- script.js
+++ script.js
@@ -83,3 +83,19 @@ switch (n) {
     console.log("小吉です");
     break;
 }
+
+const n = 4;
+switch (n) {
+  case 1:
+    console.log("大吉です");
+    break;
+  case 2:
+    console.log("吉です");
+    break;
+  case 3:
+    console.log("小吉です");
+    break;
+  default:
+    console.log("凶です");
+    break;
+}
