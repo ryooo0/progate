2de51e7eee1792a3edf81104abc4984b7a3b5330
ryooookada <btbl70120@gmail.com>
2022-01-30 19:28:38 +0900

    practice

--- script.js
+++ script.js
@@ -140,3 +140,9 @@ console.log(animals[2]);
 for (let i = 0; i < 3; i++) {
   console.log(animals[i]);
 }
+
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
+console.log(animals.length);
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
