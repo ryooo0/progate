e40bc9f7fbb68035c8bf94439a8e5f984a470a05
ryooookada <btbl70120@gmail.com>
2022-01-31 21:27:58 +0900

    practice

--- script.js
+++ script.js
@@ -199,4 +199,28 @@ const greet = (name) => {
 };
 greet("ひつじ仙人");
 
+const add = (number1, number2) => {
+  console.log(number1 + number2);
+};
+add(5, 7);
 
+const half = (number) => {
+  return number / 2;
+};
+const result = half(130);
+console.log(`130の半分は${result}です`);
+
+const check = (number) => {
+  return number % 3 == 0;
+};
+if (check(123)) {
+  console.log("3の倍数です");
+} else {
+  console.log("3の倍数ではありません");
+}
+
+const toMinutes = (hour, minute) => {
+  return hour * 60 + minute;
+}
+const result = toMinutes(3, 20);
+console.log(`${result}分`);
