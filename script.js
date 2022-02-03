4e306f7c383ef95bc21a0a06f61cf6af1ca65544
ryooookada <btbl70120@gmail.com>
2022-02-03 21:14:29 +0900

    practice

--- script.js
+++ script.js
@@ -241,3 +241,13 @@ class Animal {
   }
 }
 const animal = new Animal();
+
+class Animal {
+  constructor() {
+    this.name = "レオ";
+    this.age = 3;
+  }
+}
+const animal = new Animal();
+console.log(`名前: ${animal.name}`);
+console.log(`年齢: ${animal.age}`);
