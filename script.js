0765ae0cb844c077caa409f4b4a91675a19a6f9d
ryooookada <btbl70120@gmail.com>
2022-02-12 14:22:24 +0900

    practice

--- script.js
+++ script.js
@@ -479,3 +479,21 @@ const underTwenty = characters.filter((character) => {
   return character.age < 20
 });
 console.log(underTwenty);
+
+
+const numbers = [1, 2, 3, 4];
+const doubledNumbers = numbers.map((number) => {
+  return number * 2
+});
+console.log(doubledNumbers);
+
+const names = [
+  {firstName: "Kate", lastName: "Jones"},
+	{firstName: "John", lastName: "Smith"},
+	{firstName: "Denis", lastName: "Williams"},
+	{firstName: "David", lastName: "Black"}
+];
+const fullNames = names.map((name) => {
+  return name.firstName + name.lastName
+});
+console.log(fullNames);
