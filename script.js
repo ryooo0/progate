de2cd2925ec4a030c86b0f0e1addc99c8c1bc7f4
ryooookada <btbl70120@gmail.com>
2022-02-12 14:09:06 +0900

    practice

--- script.js
+++ script.js
@@ -445,3 +445,20 @@ console.log(characters);
 
 const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人", "とりずきん"];
 characters.forEach((character) => {console.log(character)});
+
+const numbers = [1, 3, 5, 7, 9];
+const foundNumber = numbers.find((number) => {
+  return number % 3 === 0
+});
+console.log(foundNumber);
+
+const characters = [
+  {id: 1, name: "にんじゃわんこ", age: 6},
+  {id: 2, name: "ベイビーわんこ", age: 2},
+  {id: 3, name: "ひつじ仙人", age: 100},
+  {id: 4, name: "とりずきん", age: 21}
+];
+const foundCharacter = characters.find((character) => {
+  return character.id === 3
+});
+console.log(foundCharacter);
