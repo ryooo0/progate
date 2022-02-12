bbc0dd316583867d3d06b7ade73df81e70757276
ryooookada <btbl70120@gmail.com>
2022-02-12 14:14:11 +0900

    practice

--- script.js
+++ script.js
@@ -462,3 +462,20 @@ const foundCharacter = characters.find((character) => {
   return character.id === 3
 });
 console.log(foundCharacter);
+
+
+const numbers = [1, 2, 3, 4];
+const evenNumbers = numbers.filter((number) => {
+  return number % 2 === 0
+});
+console.log(evenNumbers);
+
+const characters = [
+  {id: 1, name:"にんじゃわんこ", age: 14},
+  {id: 2, name:"ベイビーわんこ", age: 5},
+  {id: 3, name:"ひつじ仙人", age: 100}
+];
+const underTwenty = characters.filter((character) => {
+  return character.age < 20
+});
+console.log(underTwenty);
