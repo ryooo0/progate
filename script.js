d6b70358c3d7e429abf125cadb6abd22d03229a9
ryooookada <btbl70120@gmail.com>
2022-02-15 18:10:44 +0900

    practice

--- script.js
+++ script.js
@@ -507,3 +507,17 @@ const call = (callback) => {
   callback();
 };
 call(printHitsuji);
+
+
+// コールバック関数を引数の中で定義
+const printWanko = () => {
+  console.log("にんじゃわんこ");
+};
+const call = (callback) => {
+  console.log("コールバック関数を呼び出します。");
+  callback();
+};
+call(printWanko);
+call(() => {
+  console.log("ひつじ仙人");
+});
